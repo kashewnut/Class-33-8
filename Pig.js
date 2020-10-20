cd65c926ce09a2780335e05f36478881a598c92d
kashewnut <71732297+kashewnut@users.noreply.github.com>
2020-10-20 16:54:42 -0400

    Add files via upload

---
+++ Pig.js
@@ -0,0 +1,8 @@
+class Pig extends BaseClass
+{
+    constructor(x, y) {
+    super(x,y,70,50);
+    this.image=loadImage("sprites/enemy.png");
+}
+    }
+  
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Class-33-8
+# angryBirdsStage2StudentActivity
+Student Activity Link for Angry Birds Stage 2
